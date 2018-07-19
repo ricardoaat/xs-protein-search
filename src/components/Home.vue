c037f658427211858e1dc6d8cb7edb6c4545f771
Ric Arcila <ricartomojo@gmail.com>
2018-07-19 13:48:00 +0100

    Added schema json-ld

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,7 +10,8 @@
                 v-for="(protein, index) in proteins"
                 v-bind:protein = "protein"
                 :key="index"
-                class="mt-3 elevation-7"
+                class="mt-3"
+                raised
                 >
 
                 <v-card-title primary-title>
@@ -18,9 +19,11 @@
                     <h3 class="headline mb-0">{{protein.protein.recommendedName.fullName.value}}</h3>
                     <div v-if="typeof protein.proteinExistence !== 'undefined'">{{protein.proteinExistence}}</div>
                   </div>
-                  <v-btn v-on:click="protein.show = !protein.show" fab dark small>
-                    <v-icon dark>list</v-icon>
-                  </v-btn>
+                  <v-layout align-center justify-end row fill-height>
+                    <v-btn v-on:click="protein.show = !protein.show" fab dark small class="ml-4">
+                      <v-icon>mdi-arrow-down-drop-circle</v-icon>
+                    </v-btn>
+                  </v-layout>
                 </v-card-title>
                 
                 <v-card-actions>
@@ -28,11 +31,11 @@
                     
                     <div class="detail pa-3" v-if="protein.show">
                       <v-layout align-center justify-space-around="" row>
-                      <div class="property">
+                      <div class="property px-3 py-1">
                         <div class="label">Organism</div>
                         <div class="value" v-if="typeof protein.organism !== 'undefined'">{{protein.organism.names[0].value}}</div>
                       </div>
-                      <div class="property">
+                      <div class="property px-3 py-1">
                         <div class="label">Gene</div>
                         <div class="value" v-if="typeof protein.gene[0] !== 'undefined'">{{protein.gene[0].name.value}}</div>   
                       </div>
@@ -48,55 +51,81 @@
           </v-content>
       </v-layout>
     </v-slide-y-transition>
+    <script type="application/ld+json" v-text="schemaData"></script>
   </v-container>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* Enter and leave animations can use different */
-/* durations and timing functions.              */
-.slide-fade-enter-active {
-  transition: all .3s ease;
-}
-.slide-fade-leave-active {
-  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-}
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active below version 2.1.8 */ {
-  transform: translateX(10px);
-  opacity: 0;
-}
+  .slide-fade-enter-active {
+    transition: all .3s ease;
+  }
+  .slide-fade-leave-active {
+    transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+  .slide-fade-enter, .slide-fade-leave-to
+  /* .slide-fade-leave-active below version 2.1.8 */ {
+    transform: translateX(10px);
+    opacity: 0;
+  }
 
-.detail {
-  width: 100%;
-}
+  .detail {
+    width: 100%;
+  }
 
-.property {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  padding: 0px 4px;
-  border-radius: 7px;  
-}
+  .property {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    border-radius: 7px;  
+  }
 
-.detail .property .label {
-  font-weight: bold;
-}
+  .detail .property .label {
+    font-weight: bold;
+  }
 </style>
 
 <script>
 import Vue from 'vue'
 export default Vue.component('Home', {
-  props: ['protein'],
   data () {
     return {
-      message: ''
+      proteinsData: []
     }
   },
-  created () {
+  beforeCreate () {
     this.$store.dispatch('loadProteins')
+  },
+  created () {
+    this.proteinsData = this.getProteins
+    console.log(this.proteinsData)
   },
   computed: {
     proteins () {
       return this.$store.getters.proteins
+    },
+    schemaData () {
+      let schema = []
+      this.proteins.forEach((protein) => {
+        /* eslint-disable quotes */
+        schema.push(
+          {
+            "@context": [
+              "http://schema.org",
+              "http://bioschemas.org/specifications"
+            ],
+            "@type": "DataRecord",
+            "identifier": protein.id
+          }
+        )
+      })
+      return schema
+    }
+  },
+  methods: {
+
+  },
+  watch: {
+    proteins: function (newVal, oldVal) {
+      console.log(newVal)
     }
   }
 })
